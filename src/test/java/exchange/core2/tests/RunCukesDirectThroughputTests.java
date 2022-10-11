7b348d7b5bbfe97700c2eed6b192a499d2f2c638
qct <tango.qct@gmail.com>
2022-10-11 19:47:50 +0800

    [testcase] Add account.feature, symbol.feature, fee.feature to RunCukesDirectLatencyTests & RunCukesDirectThroughputTests

--- src/test/java/exchange/core2/tests/RunCukesDirectThroughputTests.java
+++ src/test/java/exchange/core2/tests/RunCukesDirectThroughputTests.java
@@ -20,7 +20,10 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResources({
     @SelectClasspathResource("exchange/core2/tests/features/basic.feature"),
-    @SelectClasspathResource("exchange/core2/tests/features/risk.feature")
+    @SelectClasspathResource("exchange/core2/tests/features/risk.feature"),
+    @SelectClasspathResource("exchange/core2/tests/features/account.feature"),
+    @SelectClasspathResource("exchange/core2/tests/features/symbol.feature"),
+    @SelectClasspathResource("exchange/core2/tests/features/fee.feature")
 })
 @ConfigurationParameters({
     @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber/cucumber.html, exchange.core2.tests.RunCukesDirectThroughputTests$CukeNaiveLifeCycleHandler"),
